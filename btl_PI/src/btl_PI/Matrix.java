df0f697c832b7cb3b4593b631bb3a69148efbaf7
Joker <Joker@192.168.5.54>
2015-05-20 16:22:48 +0700

    Chính

--- btl_PI/src/btl_PI/Matrix.java
+++ btl_PI/src/btl_PI/Matrix.java
@@ -1,5 +1,8 @@
 package btl_PI;
-
+/*
+ * Class Matrix tao 1 matran vs kich thuoc m x n à cac phuong thuc tao gia tri cac phan tu cua ma tran 
+ * va cac phuong thuc thuat toan an 2 diem
+ */
 public class Matrix {
-//matix
+
 }
