6f2be253aae742e9e7d250ceb136fa7391e14eab
binhlv2607 <binhlv2607@gmail.com>
2015-05-20 16:41:36 +0700

    // them ham khoi tao ma tran vao lop matrix

--- btl_PI/src/btl_PI/Matrix.java
+++ btl_PI/src/btl_PI/Matrix.java
@@ -1,8 +1,26 @@
 package btl_PI;
 /*
- * Class Matrix tao 1 matran vs kich thuoc m x n à cac phuong thuc tao gia tri cac phan tu cua ma tran 
+ * Class Matrix tao 1 matran vs kich thuoc m x n ï¿½ cac phuong thuc tao gia tri cac phan tu cua ma tran 
  * va cac phuong thuc thuat toan an 2 diem
  */
 public class Matrix {
+	
+	/*
+	 * ham khoi tao ma tran
+	 */
+	private int[][] matrix;
+	int row, col;
+	public Matrix(int n, int m) {
+		this.row = n;
+		this.col = m;
+		this.matrix = new int[row][col];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++)
+				this.matrix[i][j] = -1;
+		}
+	}
+	public int[][] getMatrix() {
+		return this.matrix;
+	}
 
 }
